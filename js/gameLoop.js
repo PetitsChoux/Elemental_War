45275e8dd07e1f715418eb1fe0df99658f479aa8
kabarudonphan <g.duplaix.perso@gmail.com>
2014-06-05 23:11:55 +0200

    Charging Bar 2.0

--- js/gameLoop.js
+++ js/gameLoop.js
@@ -1,4 +1,4 @@
-function progress(percent, $element) {
+var progress = function(percent, $element) {
     var progressBarWidth = percent * $element.width() / 100;
     $element.find('div').animate({ width: progressBarWidth }, 500).html(percent + "%&nbsp;");
 }
